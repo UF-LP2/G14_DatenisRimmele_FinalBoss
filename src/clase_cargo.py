08f37a002cd12c696942b4434287b00264a51360
facurimmele <facurimmele@gmail.com>
2023-08-14 20:20:44 -0300

    nuevos cambios

--- src/clase_cargo.py
+++ src/clase_cargo.py
@@ -1,7 +1,5 @@
 from src.clase_ship import Ship
 
-<<<<<<< HEAD
-
 class Cargo(Ship):
     def __init__(self, cargo, quality, draft, crew):
         self.cargo = cargo
@@ -31,21 +29,3 @@ class Cargo(Ship):
         else:
             print("El cargo no merece la pena :(")
 
-
-=======
-class Cargo(Ship):
-    def __init__(self, draft, crew, cargo, quality):
-        self.cargo = cargo
-        self.quality = quality
-        Ship.__init__(self, draft, crew)
-        print("Se creo el Cargo")
-
-    def is_worth_it(self):
-        peso = self.crew * 1.5
-        if self.quality == 1:
-            peso += 3.5
-        elif self.quality == 0.5:
-            peso += 2
-        elif self.quality == 0.25:
-            peso += 0.5
->>>>>>> origin/main
