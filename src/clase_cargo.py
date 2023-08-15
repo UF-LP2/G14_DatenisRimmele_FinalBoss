187573719311a1342c09506c8e4836d1e73933d3
LucasDatenis <101877381+LucasDatenis@users.noreply.github.com>
2023-08-15 20:59:30 -0300

    Modificaion de las excepciones

--- src/clase_cargo.py
+++ src/clase_cargo.py
@@ -3,13 +3,13 @@ from src.clase_ship import Ship
 
 class Cargo(Ship):
     def __init__(self, cargo, quality, draft, crew):
-        self.cargo = cargo
-        self.quality = quality
+        self.cargo = float(cargo)
+        self.quality = float(quality)
         Ship.__init__(self, draft, crew)
         # print("Se creo el Cargo")
 
     def peso(self):
-        peso = self.draft - self.crew * 1.5  # c/tripulacion agrega 1.5 a la diferencia del peso total
+        peso = float(self.draft) - float(self.crew) * 1.5  # c/tripulacion agrega 1.5 a la diferencia del peso total
         if self.quality == 1:
             peso -= (3.5 + self.cargo)  # se le agrega el peso
         elif self.quality == 0.5:
@@ -17,16 +17,19 @@ class Cargo(Ship):
         elif self.quality == 0.25:
             peso -= (0.5 + self.cargo)
 
-        if peso < 0:
-            raise (f"El barco nro {str(self.cant_ship)} supera su limite de peso")
+        if peso <= 0:
+            raise Exception(f"El barco nro {str(self.identificaion)} supera su limite de peso")
 
         return peso
 
     def is_worth_it(self):
-        peso = Cargo.peso(self)
-        print(f"Peso botin: {peso}")
-        if peso > 20:
-            print("¡El cargo tiene un gran botin!")
-        else:
-            print("El cargo no merece la pena :(")
+        try:
+            peso = Cargo.peso(self)
+            # print(f"Peso botin: {peso}")
+            if peso > 20:
+                print("¡El cargo tiene un gran botin!")
+            else:
+                print("El cargo no merece la pena :(")
+        except Exception as e:
+            print(str(e))
 
--- src/clase_cruise.py
+++ src/clase_cruise.py
@@ -3,22 +3,25 @@ from src.clase_ship import Ship
 
 class Cruise(Ship):
     def __init__(self, passengers, draft, crew):
-        self.passengers = passengers
+        self.passengers = float(passengers)
         Ship.__init__(self, draft, crew)
         # print("Se creo el Crucero")
 
     def peso(self):
         peso = self.draft - self.passengers * 2.25 - self.crew * 1.5  # diferencia de paso
         if peso < 0:
-            raise (f"El barco nro {str(self.cant_ship)} supera su limite de peso")
+            raise Exception(f"El barco nro {str(self.identificaion)} supera su limite de peso")
         return peso
 
     def is_worth_it(self):
-        peso = Cruise.peso(self)
-        print(f"Peso botin: {peso}")
-        if peso > 20:
-            print("¡El crucero tiene un gran botin!")
-        else:
-            print("El crucero no merece la pena :(")
+        try:
+            peso = Cruise.peso(self)
+            # print(f"Peso botin: {peso}")
+            if peso > 20:
+                print("¡El crucero tiene un gran botin!")
+            else:
+                print("El crucero no merece la pena :(")
+        except Exception as e:
+            print(str(e))
 
 
