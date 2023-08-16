a856b69ec8fa10cf4668ed3d094f096a013b47ef
LucasDatenis <101877381+LucasDatenis@users.noreply.github.com>
2023-08-15 21:00:56 -0300

    Lectura del archivo csv y prueba

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ def main() -> None:
     ship = []
     with open("ships.csv", newline='') as csvfile:
         lector = csv.reader(csvfile, delimiter=',', quotechar='|')
-        next(csvfile, None)
+        # next(csvfile, None)
         for row in lector:
             if(verifico(row[0]) and verifico(row[1])) is True:
                 if row[3] == '':
-                    if verifico(row[2]) is True:
+                    if row[2] == '':
+                        aux = Ship(row[0], row[1])
+                        ship.append(aux)
+                    elif verifico(row[2]) is True:
                         aux = Cruise(row[2], row[0], row[1])
                         ship.append(aux)
-                elif (row[2] and row[3]) == '':
-                    aux = Ship(row[0], row[1])
-                    ship.append(aux)
                 else:
                     if row[2] == '':
                         if verifico(row[3]) is True:
@@ -37,11 +37,8 @@ def main() -> None:
                             ship.append(aux)
 
         for i in range(len(ship)):
-            try:
-                valor = ship[i].is_worth_it()
-                print(valor)
-            except Exception as e:
-                print(str(e))
+            # print(str(ship[i]))
+            ship[i].is_worth_it()
 
 
 if __name__ == "__main__":
